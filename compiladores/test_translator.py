f77001139b76287f4a6db31d3a0371d50b4d4782
Valter Kuhne <valter.alejandro@hotmail.com>
2024-05-02 19:50:11 -0600

    Added matrix test test_translator.py
    Se actualizo agreagnd una prueba de matrices

--- compiladores/test_translator.py
+++ compiladores/test_translator.py
@@ -1,23 +1,20 @@
 import unittest
+import translator
 from translator import process_file
 
 class TestTranslator(unittest.TestCase):
     def test_load_image(self):
         # Prueba la carga de una imagen
         process_file("instrucciones_load_image.txt")
-        # Agrega aserciones para verificar que la imagen se haya cargado correctamente
 
     def test_show_image(self):
         # Prueba mostrar una imagen
         process_file("instrucciones_show_image.txt")
-        # Agrega aserciones para verificar que la imagen se haya mostrado correctamente
+    
+    def test_gen_matrix(self):
+        # Prueba para generar una matriz
+        process_file("instrucciones_lista.txt")
 
-    def test_save_image(self):
-        # Prueba guardar una imagen
-        process_file("instrucciones_save_image.txt")
-        # Agrega aserciones para verificar que la imagen se haya guardado correctamente
-
-    # Agrega más métodos de prueba para otras características de tu gramática
 
 if __name__ == '__main__':
     unittest.main()
