ce233f91b9f9f974fb143f624044d92e4b749f76
Valter Kuhne <valter.alejandro@hotmail.com>
2024-05-02 20:18:50 -0600

    Update test_translator.py

--- compiladores/test_translator.py
+++ compiladores/test_translator.py
@@ -5,16 +5,23 @@ from translator import process_file
 class TestTranslator(unittest.TestCase):
     def test_load_image(self):
         # Prueba la carga de una imagen
-        process_file("instrucciones_load_image.txt")
+        process_file("pruebas_load_image.txt")
 
     def test_show_image(self):
         # Prueba mostrar una imagen
-        process_file("instrucciones_show_image.txt")
+        process_file("pruebas_show_image.txt")
     
     def test_gen_matrix(self):
         # Prueba para generar una matriz
         process_file("instrucciones_lista.txt")
 
+    def test_algorithms(self):
+        #Prueba para generar un histograma
+        process_file("pruebas_algoritmos.txt")
+
+    def test_histograms(self):
+        #Prueba para generar un histograma
+        process_file("pruebas_histograma.txt")
 
 if __name__ == '__main__':
     unittest.main()
